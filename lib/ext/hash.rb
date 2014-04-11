650f1dc9b465f3e8bc9b39fb1eacd74e48341928
Paulo Sousa <paulo.sousa@corp.globo.com>
2014-04-11 15:21:55 -0300

    hash.rb: not require sort in ruby >=1.9

--- lib/ext/hash.rb
+++ lib/ext/hash.rb
@@ -1,11 +1,13 @@
-class Hash
-  alias hkeys keys
+if RUBY_VERSION < '1.9' then
+  class Hash
+    alias hkeys keys
 
-  def keys
-    hkeys.sort {|a,b| a.to_s <=> b.to_s }
-  end
+    def keys
+      hkeys.sort {|a,b| a.to_s <=> b.to_s unless (a.nil? and b.nil?)}
+    end
 
-  def each
-    keys.each { |k| yield k, self[k] }
+    def each
+      keys.each { |k| yield k, self[k] if block_given? }
+    end
   end
 end
